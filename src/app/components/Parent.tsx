8e388aae51644651a522d8b735dcbf8c47f4bdb1
Zohaib <azohaibshoukat22@gmail.com>
2024-12-21 19:34:34 +0500

    Your second commit message

--- src/app/components/Parent.tsx
+++ src/app/components/Parent.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Child from "./Child";
 
 const Parent = () => {
-  let name = "ZOHAIB";
-  let age = "22";
-  let gender = "Male";
+  const name = "ZOHAIB";
+  const age = "22";
+  const gender = "Male";
   return (
     <div className="mt-10">
       <Child name={name} age={age} gender={gender} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Parent from "./components/Parent";
 
 export default function Home() {
